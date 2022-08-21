13cfd41d77c6aa675adf24dcf96883bd24ff6dd1
Rutuja Santikar <santikarrutuja@gmail.com>
2022-08-21 16:05:28 +0530

    database created

---
+++ server/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("uploads"));
+
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connected to the database!"))
+  .catch((err) => console.log(err));
+
+app.use("/api/programs", require("./routes/routes"));
+
+app.listen(port, () =>
+  console.log(`server running at 'http://localhost:${port}`)
+);
